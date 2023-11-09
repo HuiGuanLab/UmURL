37477d042b460f09499c7e8b14d6b915b14a27d3
ssk1997 <37371875+ssk1997@users.noreply.github.com>
2023-11-09 21:25:12 +0800

    Update script_pretrain.sh

--- script_pretrain.sh
+++ script_pretrain.sh
@@ -1,12 +1,28 @@
-test_name=$1
+mkdir -p ./checkpoints/ntu60_xsub
+# mkdir -p ./checkpoints/ntu60_xview
+# mkdir -p ./checkpoints/ntu120_xsub
+# mkdir -p ./checkpoints/ntu120_xset
 
-
-if [ ! -d ./checkpoints/${test_name} ];then
-    mkdir -p ./checkpoints/${test_name}
-
-fi
- CUDA_VISIBLE_DEVICES=0,1,2,3 torchrun  --nproc_per_node=4 pretrain.py   \
-  --lr 0.0005   --batch-size 1024  \
-  --checkpoint-path ./checkpoints/${test_name} \
+CUDA_VISIBLE_DEVICES=0,1,2,3 torchrun  --nproc_per_node=4 pretrain.py   \
+  --lr 0.0005   --batch-size 512  \
+  --checkpoint-path ./checkpoints/ntu60_xsub \
   --schedule 351  --epochs 451  \
-  --pre-dataset ntu120  --protocol cross_subject | tee -a ./checkpoints/${test_name}/train.log
+  --pre-dataset ntu60  --protocol cross_subject | tee -a ./checkpoints/ntu60_xsub/train.log
+
+# CUDA_VISIBLE_DEVICES=0,1,2,3 torchrun  --nproc_per_node=4 pretrain.py   \
+#   --lr 0.0005   --batch-size 1024  \
+#   --checkpoint-path ./checkpoints/ntu60_xview \
+#   --schedule 351  --epochs 451  \
+#   --pre-dataset ntu60  --protocol cross_view | tee -a ./checkpoints/ntu60_xview/train.log
+
+# CUDA_VISIBLE_DEVICES=0,1,2,3 torchrun  --nproc_per_node=4 pretrain.py   \
+#   --lr 0.0005   --batch-size 1024  \
+#   --checkpoint-path ./checkpoints/ntu120_xsub \
+#   --schedule 351  --epochs 451  \
+#   --pre-dataset ntu120  --protocol cross_subject | tee -a ./checkpoints/ntu120_xsub/train.log 
+
+# CUDA_VISIBLE_DEVICES=0,1,2,3 torchrun  --nproc_per_node=4 pretrain.py   \
+#   --lr 0.0005   --batch-size 1024  \
+#   --checkpoint-path ./checkpoints/ntu120_xset \
+#   --schedule 351  --epochs 451  \
+#   --pre-dataset ntu120  --protocol cross_setup | tee -a ./checkpoints/ntu120_xset/train.log
