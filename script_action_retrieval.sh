bafc39eb3c0ef9e6a0e11251e9fdd0807fe4e4e9
ssk1997 <37371875+ssk1997@users.noreply.github.com>
2023-11-09 21:31:51 +0800

    Update script_action_retrieval.sh

--- script_action_retrieval.sh
+++ script_action_retrieval.sh
@@ -1,8 +1,11 @@
-test_name=$1
+dir=$1
+dataset=$2
+protocol=$3
+
 
 CUDA_VISIBLE_DEVICES=0 python action_retrieval.py \
   --lr 0.03 \
   --batch-size 512 \
   --knn-neighbours 1 \
-  --pretrained  ./checkpoints/${test_name}/checkpoint_0450.pth.tar \
-  --finetune-dataset ntu120 --protocol cross_subject
+  --pretrained  ./checkpoints/${dir}/checkpoint_0450.pth.tar \
+  --finetune-dataset ${dataset} --protocol ${protocol}
